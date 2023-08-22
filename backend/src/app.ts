79b247623404210b0121ee15a0420f43a580cadc
911-Berescu-Adrian <adrian.berescu@stud.ubbcluj.ro>
2023-08-22 04:39:52 +0300

    endpoint error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,27 @@
 import "dotenv/config";
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import DestinationModel from "./models/destination";
 
 const app = express();
 
-app.get("/", async (req, res) => {
-  const destinations = await DestinationModel.find().exec();
-  res.status(200).json(destinations);
+app.get("/", async (req, res, next) => {
+  try {
+    const destinations = await DestinationModel.find().exec();
+    res.status(200).json(destinations);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use((req, res, next) => {
+  next(Error("Not found"));
+});
+
+app.use((error: unknown, req: Request, res: Response, next: NextFunction) => {
+  console.error(error);
+  let message = "Unknown error";
+  if (error instanceof Error) message = error.message;
+  res.status(500).json({ error: message });
 });
 
 export default app;
